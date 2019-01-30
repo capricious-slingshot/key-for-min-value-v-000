df7aed82fac640905d342a4b30114b8f7bf1101d
Carden Z <ephemere@protonmail.com>
2019-01-30 02:38:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(item_hash)
+	return nil if item_hash.empty?
 	x = item_hash.first
 
   item_hash.each do |item, value|
